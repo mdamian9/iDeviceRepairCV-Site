8d7a798f7032fb1f9aad73e98bee9189bda49baa
mdamian9 <m.damian9@hotmail.com>
2019-02-25 12:41:27 -0800

    Installed reactstrap / bootstrap, added Navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-import { Navbar } from 'reactstrap';
+import './components.css';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
 
 class NavbarComponent extends Component {
-    render = () => {
+
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
         return (
             <div>
-                <p>Navbar</p>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">iDeviceRepairCV</NavbarBrand>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem className="test">
+                            <NavLink href="/">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/aboutus">About Us</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/contact">Contact</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Navbar>
             </div>
         );
-    };
+    }
+
 };
 
 export default NavbarComponent;
